1e98df12e4bd4fdccbf8da0333043c189c2f4250
Sheeegits <Singhsheetal763@gmail.com>
2025-04-17 22:45:16 +0530

    Your commit message here

--- controllers/index.js
+++ controllers/index.js
@@ -8,46 +8,6 @@ import {
   ScholarshipModel,
 } from "../models/userModel.js";
 //register user api
-export const registerUser = async (req, res) => {
-  const userModel = new UserModel(req.body);
-  userModel.password = await bcrypt.hash(req.body.password, 10);
-  try {
-    const response = await userModel.save();
-    response.password = undefined;
-    return res.status(201).json({ message: "success", data: response });
-  } catch (err) {
-    return res.status(500).json({ message: "error" });
-  }
-};
-//login user api
-export const loginUser = async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-    if (!user) {
-      return res
-        .status(401)
-        .json({ message: "Auth failed,Invalid username/password" });
-    }
-    const isPassEqual = await bcrypt.compare(req.body.password, user.password);
-    if (!isPassEqual) {
-      return res
-        .status(401)
-        .json({ message: "Auth failed,Invalid username/password" });
-    }
-    const tokenObject = {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-    };
-    const jwtToken = jwt.sign(tokenObject, process.env.SECRET, {
-      expiresIn: "4h",
-    });
-
-    return res.status(200).json({ jwtToken, tokenObject });
-  } catch (err) {
-    return res.status(500).json({ message: "error", err });
-  }
-};
 
 export const userDetails = async (req, res) => {
   try {
@@ -70,71 +30,12 @@ export const userDetails = async (req, res) => {
   }
 };
 
-export const registerCollege = async (req, res) => {
-  try {
-    if (!req.body.collegepassword) {
-      return res.status(400).json({ message: "Password is required" });
-    }
 
-    const hashedPassword = await bcrypt.hash(req.body.collegepassword, 10);
-    const collegeAuthModel = new CollegeAuthModel({
-      ...req.body,
-      collegepassword: hashedPassword,
-    });
 
-    const response = await collegeAuthModel.save();
-    response.collegepassword = undefined;
+   
 
-    return res.status(201).json({ message: "success", data: response });
-  } catch (err) {
-    return res.status(500).json({ message: "error", error: err.message });
-  }
-};
-
-export const loginCollege = async (req, res) => {
-  try {
-    const college = await CollegeAuthModel.findOne({
-      collegeemail: req.body.collegeemail,
-    });
-
-    if (!college) {
-      return res
-        .status(401)
-        .json({ message: "Auth failed, Invalid username/password" });
-    }
-
-    if (!req.body.collegepassword) {
-      return res.status(400).json({ message: "Password is required" });
-    }
-
-    const isPassEqual = await bcrypt.compare(
-      req.body.collegepassword,
-      college.collegepassword
-    );
-
-    if (!isPassEqual) {
-      return res
-        .status(401)
-        .json({ message: "Auth failed, Invalid username/password" });
-    }
-
-    const tokenObject = {
-      _id: college._id,
-      collegename: college.collegename,
-      collegeemail: college.collegeemail,
-    };
-
-    const jwtToken = jwt.sign(tokenObject, process.env.SECRET, {
-      expiresIn: "4h",
-    });
-
-    return res.status(200).json({ jwtToken, tokenObject });
-  } catch (err) {
-    return res
-      .status(500)
-      .json({ message: "Internal Server Error", error: err.message });
-  }
-};
+   
+      
 
 
 export const reviewdetails = async (req, res) => {
